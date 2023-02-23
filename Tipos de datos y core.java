a59e5a8d940f195d274d877999584077acc09a1d
Elpatowaton <124885235+Elpatowaton@users.noreply.github.com>
2023-02-22 21:47:09 -0500

    Update Tipos de datos y core.java

--- Tipos de datos y core.java
+++ Tipos de datos y core.java
@@ -1,1 +1,65 @@
-a
+const libro = {
+  titulo: "Aprendiendo JavaScript"
+  autor: "Carlos Azaustre",
+  numPaginas: 100,
+  editorial: "carlosazuastre.es"
+  precio: "24.90",
+}
+
+libro.titulo
+libro["numPaginas"]
+libro.titulo = "Desarrollo web con React"
+Libro.titulo
+
+let pages = "numPaginas"
+libro[pages]
+libro.pages
+libro.leer()
+
+let libro = {
+  
+  titulo:"Aprendiendo JavaScript",
+  autor:{
+    
+    nombre:`felipe hernández`,
+    edad: 15,
+    redes: {
+      facebook:"felipe.h.sandoval"
+      instagram:"elpatowaton"
+      Twitch:"FELI_WAS_HERE"
+}
+libro.autor.nombre
+libro[`autor`][`redes`][`facebook`]
+
+const coche1 = {marca: "Tesla", modelo: "X"}
+const coche2 = {marce: "Tesla", modelo: "X"}
+coche1 === coche2
+coche1.marca === coche2.marca
+const coche3 = coche1
+coche3 === coche1
+
+  
+  
+  
+25
+25.5
+0x1F
+0x10
+5.4e2
+  
+1e1000
+  
+"a" / 15
+  
+let numero = new Number(5)
+  
+parseInt("15")
+parseInt("1111", 2)
+parseFloat("5e3") 
+  
+let n = 2.5678
+n.toFixed(2);
+
+  
+
+  
