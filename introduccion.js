5ec6aabde89ca1a1b7aa7e11db93ccee20fb9ab0
juanardila15 <96641054+juanardila15@users.noreply.github.com>
2023-02-22 21:49:01 -0500

    Update introduccion.js

--- introduccion.js
+++ introduccion.js
@@ -1,4 +1,4 @@
-{
+
  // OPERADORES PRINCIPALES
  let suma = 5 + 5
  suma
@@ -14,6 +14,8 @@
 
  let potencia = 3 ** 3
  potencia
+ 
+ 
  // OPERADORES POST Y PRE
  let x = 1
  x
@@ -33,6 +35,8 @@
  let b = --x
  b
  x
+ 
+ 
  //  OPERADORES VULEANOS
  !true
  !false
@@ -53,10 +57,12 @@
  2 <= 1
  'e' < 'd'
 
+
  // OPERADOR AND &&
  true && false && true && true
  0 && true
  1 && 'hello'
+
 
  // OPERADOR OR ||
 
@@ -64,15 +70,57 @@
  const port = process.env.port || 3000
  port
 
+
  // SENTENCIA IF
- let operacion = 5 + 1
- if (true) {
-  console.log('Es true')
+ let operacion = 0 + 1;
+ if (operacion > 7) {
+  console.log('Es mayor a 7')
+ } else if (operacion > 7 && operacion > 2) {
+  console.log('Esta entre 7 y 2')
  } else {
-  console.log('Es false')
- }
-}
+   console.log('es menor de 2')
+   }
+   
+   
+  // SENTENCIA SWITCH
+  let operacion = 0  
+  
+  switch(operacion) {
+   case 0:
+    console.log("El resultado es 7");
+     break;
+     default;
+      console.log("El resultado no es ni 0 ni es 7")
+      
+      
+    funtion saludar(nombre = "Carlos") {
+    return `¡Hola ${nombre}!`
+   }
+   
+   saludar()
+   saludar("Paola")
+   
+   
+   class Inventario {
+    constructor(nombre) {
+     this.nombre =nombre;
+     this.articulos = [];
+    }
+    
+    getBimbre() {
+     return this.nmbre;
+     }
+     
+     add(articulo, cantidad) {
+        this.articulos[articulo] = cantidad;
+        }
+        cantidad(articulo) {
+           return this.articulos[articulo]
+       }
+     }
+     
+     let libros = new Inventario('libros');
+     libros.getNombre()
+     libros.add("Aprendiendo JavaScript", 5);
+     libros.cantidad('Aprendiendo JavaScript');
 
-
-
-<h1>hola mundo</h1>
