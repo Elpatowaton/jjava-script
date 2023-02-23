0f205f0d120c0a24f8a5a51d0de88de036d68e2e
Elpatowaton <124885235+Elpatowaton@users.noreply.github.com>
2023-02-22 22:16:28 -0500

    Update Tipos de datos y core.java

--- Tipos de datos y core.java
+++ Tipos de datos y core.java
@@ -59,7 +59,86 @@ parseFloat("5e3")
   
 let n = 2.5678
 n.toFixed(2);
+n.toFixed(0);
+n.toExponential(2);
 
-  
+(1111).toString(10)
+(15).toString2)
 
+
+Math.PI
+Math.E
+Math.random()
+Math.random()
+Math.pow( 2, 6)
+2 ** 6
+Math.min(2, 4, 6)
+Math.max(4, 6, 2)
+
+let miArray=[1, 2, 3]
+let miArray=["hola", "como", "vas"]
+let miArray=
+  {propiedad: "valor"},
+  {propiedad: "valor"}
+]
+let miArray=[
+  [2, 4]
+  [3, 6]
+]
+
+miArray [0][0]
+
+
+let miArray = [
+  1,
+  true,
+  [3, 2]
+  "hola"
+  { clave: "valor"} 
+]
+
+miArray.length
+
+
+let miArray = [3, 6, 1, 4]
+miArray.sort()
+miArray.pop()
+miArray
+miArray.sort()
+miArray.reverse()
   
+let valor = 3
+const template= [
+  "<li>",
+  valor,
+  "<li>"
+].join("0000");
+console.log(template)
+  
+let miArray = [2, 4, 6, 8]
+let raices = miArray.map(Math.sqrt)
+raices
+
+let miArray = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+let resultado  miArray.filter (item => item % 3 ===0)
+resultado
+miArray
+
+
+let miArray = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+miArray.slice(2)
+miArray.slice(2, -2)
+
+
+"javascript"[2]
+"javascript".length
+"javascript".charCodeAt(2)
+"javascript".index0f("string")
+"javascript".substring(2, 4)
+
+let texto = "hola"
+let text = new String("hola")
+
+const fecha = new date()
+fecha.tostring().split(" ")[4].split(":")[1]
+
