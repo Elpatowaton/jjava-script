55a32f739375340038758426d7bce26c8098efa1
juanardila15 <96641054+juanardila15@users.noreply.github.com>
2023-02-22 21:12:40 -0500

    Add files via upload

---
+++ introduccion.js
@@ -0,0 +1,74 @@
+{
+ // OPERADORES PRINCIPALES
+ let suma = 5 + 5
+ suma
+
+ let resta = 3 - 2
+ resta
+
+ let division = 3 / 0
+ division
+
+ let multiplicacion = 5 * 10
+ multiplicacion
+
+ let potencia = 3 ** 3
+ potencia
+ // OPERADORES POST Y PRE
+ let x = 1
+ x
+
+ let y = ++x
+ y
+ x
+
+ let z = x++
+ z
+ x
+
+ let a = x--
+ a
+ x
+
+ let b = --x
+ b
+ x
+ //  OPERADORES VULEANOS
+ !true
+ !false
+
+ !!true
+
+ true === true
+ true === false
+ true !== false
+ false !== false
+
+ 3 == '3'
+ 3 === 3
+
+ 5 > 3
+ 5 < 3
+ 3 >= 1
+ 2 <= 1
+ 'e' < 'd'
+
+ // OPERADOR AND &&
+ true && false && true && true
+ 0 && true
+ 1 && 'hello'
+
+ // OPERADOR OR ||
+
+ false || false || true || false
+ const port = process.env.port || 3000
+ port
+
+ // SENTENCIA IF
+ let operacion = 5 + 1
+ if (true) {
+  console.log('Es true')
+ } else {
+  console.log('Es false')
+ }
+}
